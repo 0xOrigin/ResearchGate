e83c33566f5ce745accb44e102d7a9e99f6a3b2b
walid404 <walid.max28@gmail.com>
2022-05-02 23:01:30 +0200

    Modify FileNameGenerator Class

--- Controllers/FileNameGenerator.cs
+++ Controllers/FileNameGenerator.cs
@@ -7,15 +7,19 @@ namespace Research_Gate.Controllers
 {
     public class FileNameGenerator
     {
-        public static string AuthorPhotoName(int AuthorId)
+        public static string GenerateAuthorPhotoName(int AuthorId, string Extension)
         {
             Random random = new Random(5);
-            return AuthorId.ToString() + random.Next(100000, 999999).ToString() + ".jpg";
+            int Fnumber = random.Next(1,9);
+            int Snumber = random.Next(9);
+            string PhotoName = AuthorId.ToString() + Fnumber.ToString() + Snumber.ToString() +
+                random.Next(0000, 9999).ToString()+ (AuthorId+Fnumber + Snumber).ToString() + '.'+Extension;
+            return PhotoName;
         }
 
-        public static string PaperPhotoName(int PaperId, int AuthorId)
+        public static string GeneratePaperName(int PaperId, int AuthorId, string Extension)
         {
-            return PaperId + AuthorPhotoName(AuthorId);
+            return PaperId.ToString() + GenerateAuthorPhotoName(AuthorId, Extension);
         }
     }
 }
