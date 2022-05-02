2942beef553e71b2d38b44694ede74033577a0fb
walid404 <walid.max28@gmail.com>
2022-05-02 20:35:08 +0200

    Add GeneratorFileName Class

---
+++ Controllers/FileNameGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Research_Gate.Controllers
+{
+    public class FileNameGenerator
+    {
+        public static string AuthorPhotoName(int AuthorId)
+        {
+            Random random = new Random(5);
+            return AuthorId.ToString() + random.Next(100000000, 999999999).ToString() + ".jpg";
+        }
+
+        public static string PaperPhotoName(int PaperId, int AuthorId)
+        {
+            return PaperId + AuthorPhotoName(AuthorId);
+        }
+    }
+}
