eded8519fe65e4458c21f8163e22e3a7c11b9112
HadeerEmadEldeen <hadeeremadalden123@gmail.com>
2022-05-05 21:31:22 +0200

    create EditProfile method

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Research_Gate.ViewModels;
+using Research_Gate.Models;
 
 namespace Research_Gate.Controllers
 {
@@ -17,6 +18,12 @@ namespace Research_Gate.Controllers
             return View();
         }
 
+        
+        public ActionResult EditProfile(Author obj)
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult EditProfile()
         {
             return View();
