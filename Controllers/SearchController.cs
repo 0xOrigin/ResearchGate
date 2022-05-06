458183e03ecf64076025c27b752c785e77efb8ec
0xOrigin <egyahmed.ezzat120@gmail.com>
2022-05-06 23:20:41 +0200

    Eliminate Empty inputs in search page.

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -24,33 +24,36 @@ namespace Research_Gate.Controllers
         }
 
         [HttpPost]
-        [Route("Search/SearchByName/{name}")]
-        public PartialViewResult SearchByName(string name)
+        [Route("Search/SearchByName/{name?}")]
+        public ActionResult SearchByName(string name)
         {
             ClearResults();
-            if (name != null)
+
+            if (name != null && name.Trim() != "")
                 authors = dbContext.Authors.Where(a => (a.Fname + a.Lname).Contains(name)).ToList();
 
             return PartialView("_Search", authors);
         }
 
         [HttpPost]
-        [Route("Search/SearchByEmail/{Email}")]
-        public PartialViewResult SearchByEmail(string Email)
+        [Route("Search/SearchByEmail/{Email?}")]
+        public ActionResult SearchByEmail(string Email)
         {
             ClearResults();
-            if(Email != null)
+
+            if (Email != null && Email.Trim() != "")
                 authors = dbContext.Authors.Where(a => a.Email.Contains(Email)).ToList();
             
             return PartialView("_Search", authors);
         }
 
         [HttpPost]
-        [Route("Search/SearchByUniversity/{university}")]
-        public PartialViewResult SearchByUniversity(string university)
+        [Route("Search/SearchByUniversity/{university?}")]
+        public ActionResult SearchByUniversity(string university)
         {
             ClearResults();
-            if(university != null)
+
+            if (university != null && university.Trim() != "")
                 authors = dbContext.Authors.Where(a => a.University.Contains(university)).ToList();
             
             return PartialView("_Search", authors);
